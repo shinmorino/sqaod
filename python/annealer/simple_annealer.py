fix : energy calculation is fixed to include all qbit interactions.

python/annealer/simple_annealer.py
@@ -71,8 +71,16 @@ class SimpleAnnealer :
         for i in range(self.N * self.m):
             x = np.random.randint(N)
             y = np.random.randint(m)
-            dE = (2*q[y][x]*(h[x]+q[y][(N+x-1)%N]*J[x][(N+x-1)%N]+q[y][(x+1)%N]*J[x][(x+1)%N]))*1.0/m
-            dE += -q[y][x]*(q[(m+y-1)%m][x]+q[(y+1)%m][x])*np.log(np.tanh(G/kT/m))*1.0/kT
+
+            qyx = q[y][x]
+            sum = 0
+            for i in range(x) :
+                sum += q[y][i] * J[x][i]
+            for i in range(x + 1, N) :
+                sum += q[y][i] * J[x][i]
+
+            dE = (2 * qyx * (h[x] + sum)) * 1.0 / m
+            dE += -qyx * (q[(m + y - 1) % m][x] + q[(y + 1) % m][x]) * np.log(np.tanh(G/kT/m)) * 1.0 / kT
             if np.exp(-dE/kT)> np.random.rand():
                 q[y][x] = -q[y][x]
 