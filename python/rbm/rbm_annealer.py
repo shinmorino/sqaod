48f72bde9a143f35360fef7849f89d04169ab5e3
Shinya Morino <shin.morino@gmail.com>
2017-11-26 14:38:15 +0900

    first working version with a doc for formulas, while needing further validation.

--- python/rbm/rbm_annealer.py
+++ python/rbm/rbm_annealer.py
@@ -32,8 +32,8 @@ class RBMAnnealer :
 
         self._c = (1. / 4.) * np.sum(W) + (1. / 2.) * (np.sum(b0) + np.sum(b1))
         self._J = W * (1. /  4.)
-        h0 = [np.sum(W[:, i]) - 0.5 * b0[i] for i in range(0, N0)]
-        h1 = [np.sum(W[i]) - 0.5 * b1[i] for i in range(0, N1)]
+        h0 = [np.sum(W[:, i]) + 0.5 * b0[i] for i in range(0, N0)]
+        h1 = [np.sum(W[j]) + 0.5 * b1[j] for j in range(0, N1)]
         self._hlist = [h0, h1]
         
     def set_ising_model(self, J, h0, h1, c = 0.) :
@@ -63,30 +63,30 @@ class RBMAnnealer :
         for v in np.nditer(q, [], ['readwrite']):
             v[...] = np.random.choice([-1,1])
     
-    def get_J(self) :
-        return self._J
-
-    def get_h(self, idx) :
-        return self._hlist[idx]
+    def get_hJc(self) :
+        return self._h, self._J, self._c
         
     def get_E(self) :
         return self.E;
 
     def _anneal_half_step(self, N, qAnneal, h, J, qFixed, G, kT) :
         dEmat = np.matmul(J, qFixed.T)
-        twoDivM = 2 / m
+        twoDivM = 2. / m
         tempCoef = np.log(np.tanh(G/kT/m)) / kT
         invKT = 1. / kT
         for im in range(self.m):
+            rim = np.random.randint(m)
             for iq in range(N):
-                q = qAnneal[im][iq]
-                dE = twoDivM * q * (h[iq] + dEmat[iq, im])
-                mNeibour0 = (im + m - 1) % m
-                mNeibour1 = (im + 1) % m
-                dE += -q * (qAnneal[mNeibour0][iq] + qAnneal[mNeibour1][iq]) * tempCoef
+                riq = iq #np.random.randint(N)
+
+                q = qAnneal[rim][riq]
+                dE = twoDivM * q * (h[riq] + dEmat[riq, rim])
+                mNeibour0 = (rim + m - 1) % m
+                mNeibour1 = (rim + 1) % m
+                dE += -q * (qAnneal[mNeibour0][riq] + qAnneal[mNeibour1][riq]) * tempCoef
                 thresh = 1 if dE < 0 else np.exp(- dE * invKT) 
                 if thresh > np.random.rand():
-                    qAnneal[im][iq] = -q
+                    qAnneal[rim][riq] = -q
 
     def _get_vars(self) :
         return self._J, self._hlist[0], self._hlist[1], self._qlist[0], self._qlist[1]
@@ -94,12 +94,12 @@ class RBMAnnealer :
     def anneal_one_step(self, G, kT) :
         J, h0, h1, q0, q1 = self._get_vars()
         self._anneal_half_step(N1, q1, h1, J, q0, G, kT)
-        self._anneal_half_step(N0, q0, h0, np.transpose(J), q1, G, kT)
+        self._anneal_half_step(N0, q0, h0, J.T, q1, G, kT)
 
     def calculate_E(self) :
         J, h0, h1, q0, q1 = self._get_vars()
-        J = self._J
-        self.E = np.dot(h0, q0[0]) + np.dot(h1, q1[0]) + np.dot(h1, np.matmul(J, q0[0]))
+        self.E = - np.dot(h0, q0[0]) - np.dot(h1, q1[0]) - np.dot(h1, np.matmul(J, q0[0]))
+        return self.E
         
 
 def rbm_annealer(N0 = 0, N1 = 0, m = 0) :
@@ -107,37 +107,36 @@ def rbm_annealer(N0 = 0, N1 = 0, m = 0) :
 
 
 if __name__ == '__main__' :
-    N0 = 10
-    N1 = 8
-    m = 5
+    N0 = 6
+    N1 = 6
+    m = 4
     
-    np.random.seed(10000)
+    np.random.seed(0)
         
     an = rbm_annealer(N0, N1, m)
-    W = 2 * np.random.random((N1, N0)) - 1.
-    b0 = 2 * np.random.random((N0)) - 1.
-    b1 = 2 * np.random.random((N1)) - 1.
     
+    W = np.random.random((N1, N0)) - 0.5
+    b0 = np.random.random((N0)) - 0.5
+    b1 = np.random.random((N1)) - 0.5
     an.set_qubo(W, b0, b1)
-    an.randomize_q(0)
-    an.randomize_q(1)
+    #an.set_ising_model(W, b0, b1)
 
     Ginit = 5.
     Gfin = 0.01
 
-    nRepeat = 4
+    nRepeat = 30
     kT = 0.02
     tau = 0.99
 
     for loop in range(0, nRepeat) :
+        an.randomize_q(0)
+        an.randomize_q(1)
         G = Ginit
         while Gfin < G :
             an.anneal_one_step(G, kT)
-            an.calculate_E()
             G = G * tau
 
-    q0 = an.get_q(0) 
-    q1 = an.get_q(1) 
-    E = an.get_E() 
-    print(q0[0,:], q1[0,:], E)
-    
+        q0 = an.get_q(0) 
+        q1 = an.get_q(1) 
+        E = an.calculate_E()
+        print(q0[0,:], q1[0,:], E)
