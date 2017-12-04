914601d89f888e469d24967a4111bf812d7a7c5b
Shinya Morino <shin.morino@gmail.com>
2017-12-05 01:54:46 +0900

    adding rbm functions.

--- src/cpu/Traits.cpp
+++ src/cpu/Traits.cpp
@@ -1,4 +1,3 @@
-#include "types.h"
 #include "Traits.h"
 #include <iostream>
 
@@ -75,7 +74,7 @@ void SolverTraits<real>::denseGraphCalculate_E_fromQbits(real *E,
     const Eigen::Map<Matrix> eh((real*)h, 1, N);
     const Eigen::Map<Matrix> eJ((real*)J, N, N);
     const Matrix eq = utils<real>::bitsToMat(q, N, 1);
-    Eigen::Map<Matrix>(E, 1, 1) = Matrix::Constant(1, 1, c) + eh * eq + eq.transpose() * (eJ * eq);
+    *E = c + (eh * eq + eq.transpose() * (eJ * eq))(0, 0);
 }
     
     
@@ -93,7 +92,8 @@ void SolverTraits<real>::denseGraphBatchCalculate_E_fromQbits(real *E,
 
     Matrix tmp = eJ * eq.transpose();
     Matrix sum = tmp.cwiseProduct(eq.transpose()).colwise().sum(); /* batched dot product. */
-    eE = Matrix::Constant(1, nBatch, c) + eh * eq.transpose() + sum;
+    eE = eh * eq.transpose() + sum;
+    eE.array() += c;
 }
 
 
@@ -104,12 +104,13 @@ void SolverTraits<real>::rbmCalculate_E(real *E,
                                         const real *b0, const real *b1, const real *W,
                                         const char *x0, const char *x1,
                                         int N0, int N1) {
-    const Eigen::Map<Matrix> eb0((real*)b0, N0, 1);
-    const Eigen::Map<Matrix> eb1((real*)b1, N1, 1);
+    const Eigen::Map<Matrix> eb0((real*)b0, 1, N0);
+    const Eigen::Map<Matrix> eb1((real*)b1, 1, N1);
     const Eigen::Map<Matrix> eW((real*)W, N1, N0);
     const Matrix ex0 = utils<real>::bitsToMat(x0, N0, 1);
     const Matrix ex1 = utils<real>::bitsToMat(x1, N1, 1);
     Eigen::Map<Matrix> eE(E, 1, 1);
+    Matrix prod = (eW * ex0);
     eE = - eb0 * ex0 - eb1 * ex1 - ex1.transpose() * (eW * ex0);
 }
 
@@ -118,96 +119,73 @@ void SolverTraits<real>::rbmBatchCalculate_E(real *E,
                                              const real *b0, const real *b1, const real *W,
                                              const char *x0, const char *x1,
                                              int N0, int N1, int nBatch0, int nBatch1) {
-    const Eigen::Map<Matrix> eb0((real*)b0, N0, 1);
-    const Eigen::Map<Matrix> eb1((real*)b1, N1, 1);
+    const Eigen::Map<Matrix> eb0((real*)b0, 1, N0);
+    const Eigen::Map<Matrix> eb1((real*)b1, 1, N1);
     const Eigen::Map<Matrix> eW((real*)W, N1, N0);
     const Matrix ex0 = utils<real>::bitsToMat(x0, nBatch0, N0);
     const Matrix ex1 = utils<real>::bitsToMat(x1, nBatch1, N1);
     Eigen::Map<Matrix> eE(E, nBatch1, nBatch0);
-    eE = - ex1 * (eW * ex0.transpose());
-    // Eigen::Matrix<real, 1, Eigen::Dynamic> v0 = eb0 * ex0.transpose();
-    eE -= eb0 * ex0.transpose();
-    eE -= (eb1 * ex1.transpose()).transpose();
-    /* - np.matmul(b0, x0.T).reshape(1, iStep) - np.matmul(b1, x1.T).reshape(jStep, 1) \
-        - np.matmul(x1, np.matmul(W, x0.T))
-    */
+
+    RowVector ebx0 = eb0 * ex0.transpose();
+    ColumnVector ebx1 = (eb1 * ex1.transpose()).transpose();
+    eE.rowwise() = - ebx0;
+    eE.colwise() -=  ebx1;
+    eE -= ex1 * (eW * ex0.transpose());
 }
 
+
+template<class real>
+void SolverTraits<real>::rbmCalculate_hJc(real *h0, real *h1, real *J, real *c,
+                                          const real *b0, const real *b1, const real *W,
+                                          int N0, int N1) {
+    const Eigen::Map<Matrix> eb0((real*)b0, N0, 1), eb1((real*)b1, N1, 1), eW((real*)W, N1, N0);
+    Eigen::Map<Matrix> eh0(h0, N0, 1), eh1(h1, N1, 1);
+    Eigen::Map<Matrix> eJ(J, N1, N0);
+
+    eJ = real(0.25) * eW;
+    eh0 = real(0.25) * eW.colwise().sum().transpose() + real(0.5) * eb0;
+    eh1 = real(0.25) * eW.rowwise().sum() + real(0.5) * eb1;
+    *c = real(0.25) * eW.sum() + real(0.5) * (eb0.sum() + eb1.sum());
+}
+
+
+
+template<class real>
+void SolverTraits<real>::
+rbmCalculate_E_fromQbits(real *E,
+                         const real *h0, const real *h1, const real *J, real c,
+                         const char *q0, const char *q1,
+                         int N0, int N1) {
+    const Eigen::Map<Matrix> eh0((real*)h0, 1, N0), eh1((real*)h1, 1, N1);
+    const Eigen::Map<Matrix> eJ((real*)J, N1, N0);
+    const Matrix eq0 = utils<real>::bitsToMat(q0, N0, 1);
+    const Matrix eq1 = utils<real>::bitsToMat(q1, N1, 1);
+    *E = (- eh0 * eq0 - eh1 * eq1 - eq1.transpose() * (eJ * eq0))(0, 0) - c;
+}
+    
+
+template<class real>
+void SolverTraits<real>::
+rbmBatchCalculate_E_fromQbits(real *E,
+                              const real *h0, const real *h1, const real *J, real c,
+                              const char *q0, const char *q1,
+                              int N0, int N1, int nBatch0, int nBatch1) {
+    const Eigen::Map<Matrix> eh0((real*)h0, 1, N0);
+    const Eigen::Map<Matrix> eh1((real*)h1, 1, N1);
+    const Eigen::Map<Matrix> eJ((real*)J, N1, N0);
+    const Matrix eq0 = utils<real>::bitsToMat(q0, nBatch0, N0);
+    const Matrix eq1 = utils<real>::bitsToMat(q1, nBatch1, N1);
+    Eigen::Map<Matrix> eE(E, nBatch1, nBatch0);
+
+    RowVector ehq0 = eh0 * eq0.transpose();
+    ColumnVector ehq1 = (eh1 * eq1.transpose()).transpose();
+    eE.rowwise() = - ehq0;
+    eE.colwise() -=  ehq1;
+    eE -= eq1 * (eJ * eq0.transpose());
+    eE.array() -= c;
+}
 
 #if 0
-
-
-template<class real>
-void SolverTraits<real>::denseGraphCalculate_hJc(real *h, real *J, real *c, const real *W, int N) {
-    THROW_IF(!utils<real>::isSymmetric(W, N), "W is not symmetric.");
-
-    Eigen::Map<Matrix> eW((real*)W, N, N);
-    Eigen::Map<Matrix> eh(h, 1, N);
-    Eigen::Map<Matrix> eJ(J, N, N);
-    
-    eh = real(0.5) * eW.colwise().sum();
-
-    eJ = 0.25 * eW;
-    real eJsum = eJ.sum();
-    real diagSum = eJ.diagonal().sum();
-    for (int i = 0; i < N; ++i)
-        eJ(i, i) = real(0.);
-    *c = eJsum + diagSum;
-}
-
-template<class real>
-void SolverTraits<real>::denseGraphCalculate_E_fromQbits(real *E,
-                                                         const real *h, const real *J, real c,
-                                                         const char *q, int N) {
-    const Eigen::Map<Matrix> eh((real*)h, 1, N);
-    const Eigen::Map<Matrix> eJ((real*)J, N, N);
-    const Matrix eq = utils<real>::bitsToMat(q, N, 1);
-    Eigen::Map<Matrix>(E, 1, 1) = Matrix::Constant(1, 1, c) + eh * eq + eq.transpose() * (eJ * eq);
-}
-    
-    
-
-template<class real>
-void SolverTraits<real>::denseGraphBatchCalculate_E_fromQbits(real *E,
-                                                              const real *h, const real *J, real c,
-                                                              const char *q, int N, int nBatch) {
-    const Eigen::Map<Matrix> eh((real*)h, 1, N);
-    const Eigen::Map<Matrix> eJ((real*)J, N, N);
-    Eigen::Map<Matrix> Ex(E, N, 1);
-
-    const Matrix eq = utils<real>::bitsToMat(q, nBatch, N);
-    Eigen::Map<Matrix> eE(E, 1, nBatch);
-
-    Matrix tmp = eJ * eq.transpose();
-    Matrix sum = tmp.cwiseProduct(eq.transpose()).colwise().sum(); /* batched dot product. */
-    eE = Matrix::Constant(1, nBatch, c) + eh * eq.transpose() + sum;
-}
-
-
-
-def rbm_calculate_E(W, x0, x1) :
-    return - np.dot(b0, x0) - np.dot(b1, x1) - np.dot(x1, np.matmul(W, x0))
-
-def rbm_batch_calculate_E(W, x0, x1) :
-    return - np.matmul(b0, x0.T).reshape(1, iStep) - np.matmul(b1, x1.T).reshape(jStep, 1) \
-        - np.matmul(x1, np.matmul(W, x0.T))
-
-
-def rbm_calculate_hJc(W) :
-    N0 = W.shape[1]
-    N1 = W.shape[0]
-    
-    c = 0.25 * np.sum(W) + 0.5 * (np.sum(b0) + np.sum(b1))
-    J = 0.25 * W
-    h0 = [(1. / 4.) * np.sum(W[:, i]) + 0.5 * b0[i] for i in range(0, N0)]
-    h1 = [(1. / 4.) * np.sum(W[j]) + 0.5 * b1[j] for j in range(0, N1)]
-    hlist = [h0, h1]
-
-    return hlist, J, c
-
-    
-def rbm_calculate_E_from_qbits(h, J, c, q0, q1) :
-    return - np.dot(h0, q0) - np.dot(h1, q1) - np.dot(q1[0], np.matmul(J, q0[0])) - c
 
 def rbm_batch_calculate_E_from_qbits(h, J, c, q0, q1) :
     return - np.matmul(h0, q0.T).reshape(1, q0.shape[0]) \
--- src/cpu/Traits.h
+++ src/cpu/Traits.h
@@ -11,8 +11,7 @@ namespace quantd_cpu {
 
 template<class real>
 struct utils {
-    typedef Eigen::Matrix<real, Eigen::Dynamic, Eigen::Dynamic> Matrix;
-    typedef Eigen::Array<real, Eigen::Dynamic, Eigen::Dynamic> Array;
+    typedef Eigen::Matrix<real, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> Matrix;
 
     static
     bool isSymmetric(const real *W, int N);
@@ -24,8 +23,9 @@ struct utils {
 
 template<class real>
 struct SolverTraits {
-    typedef Eigen::Matrix<real, Eigen::Dynamic, Eigen::Dynamic> Matrix;
-    typedef Eigen::Array<real, Eigen::Dynamic, Eigen::Dynamic> Array;
+    typedef Eigen::Matrix<real, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> Matrix;
+    typedef Eigen::Matrix<real, 1, Eigen::Dynamic> RowVector;
+    typedef Eigen::Matrix<real, Eigen::Dynamic, 1> ColumnVector;
 
     static
     void denseGraphCalculate_E(real *E, const real *W, const char *x, int N);
@@ -61,17 +61,21 @@ struct SolverTraits {
                              int N0, int N1, int nBatch0, int nBatch1);
 
     static
-    void rbmCalculate_hJc(real *h, real *J, real *c, const real *W, int N);
+    void rbmCalculate_hJc(real *h0, real *h1, real *J, real *c,
+                          const real *b0, const real *b1, const real *W,
+                          int N0, int N1);
 
     static
     void rbmCalculate_E_fromQbits(real *E,
-                                  const real *h, const real *J, real c, const char *q,
-                                  int N);
+                                  const real *h0, const real *h1, const real *J, real c,
+                                  const char *q0, const char *q1,
+                                  int N0, int N1);
 
     static
     void rbmBatchCalculate_E_fromQbits(real *E,
-                                       const real *h, const real *J, real c, const char *q,
-                                       int N, int nBatch);
+                                       const real *h0, const real *h1, const real *J, real c,
+                                       const char *q0, const char *q1,
+                                       int N0, int N1, int nBatch0, int nBatch1);
 
     
 };
