1f67190bdbd758c65d9c018c9055993dfbd27658
Shinya Morino <shin.morino@gmail.com>
2017-12-17 13:20:18 +0900

    adding PyObject allocation methods on Sclar objects.

--- include/pyglue.h
+++ include/pyglue.h
@@ -9,11 +9,12 @@
 
 template<class real>
 struct NpMatrixT {
+    NpMatrixT() { }
     NpMatrixT(PyObject *obj);
     
     real *data;
     int nDims;
-    int dims[2];
+    npy_intp dims[2];
 
     /* accessor for ease of coding. */
     operator real*() {
@@ -27,7 +28,7 @@ struct NpMatrixT {
 template<class real>
 inline
 NpMatrixT<real>::NpMatrixT(PyObject *obj) {
-    dims[0] = dims[1] = -1;
+    dims[0] = dims[1] = 1;
     PyArrayObject *arr = (PyArrayObject*)obj;
     data = (real*)PyArray_DATA(arr);
     nDims = PyArray_NDIM(arr);
@@ -36,7 +37,20 @@ NpMatrixT<real>::NpMatrixT(PyObject *obj) {
 }
 
 
-typedef NpMatrixT<char> NpBitMatrix;
+struct NpBitMatrix : public NpMatrixT<char> {
+    NpBitMatrix() { }
+    NpBitMatrix(PyObject *obj) : NpMatrixT<char>(obj) { }
+    void allocate(int nRows, int nCols) {
+        /* new array object */
+        dims[0] = nRows;
+        dims[1] = nCols;
+        obj = PyArray_EMPTY(2, dims, NPY_INT8, 0);
+        PyArrayObject *arr = (PyArrayObject*)obj;
+        /* setup members */
+        data = (char*)PyArray_DATA(arr);
+    }
+    PyObject *obj;
+};
 
 
 
@@ -64,6 +78,33 @@ template<> inline
 NpConstScalarT<float>::NpConstScalarT(PyObject *obj) {
     PyFloat32ScalarObject *fpObj = (PyFloat32ScalarObject*)obj;
     data = fpObj->obval;
+}
+
+
+inline
+PyObject *newScalarObj(double v) {
+    PyObject *obj = PyArrayScalar_New(Float64);
+    PyArrayScalar_ASSIGN(obj, Float64, (npy_float64)v);
+    return obj;
+}
+
+inline
+PyObject *newScalarObj(float v) {
+    PyObject *obj = PyArrayScalar_New(Float32);
+    PyArrayScalar_ASSIGN(obj, Float32, (npy_float32)v);
+    return obj;
+}
+
+
+
+/* Helpers for dtypes */
+inline
+bool isFloat64(PyObject *dtype) {
+    return dtype == (PyObject*)&PyFloat64ArrType_Type;
+}
+inline
+bool isFloat32(PyObject *dtype) {
+    return dtype == (PyObject*)&PyFloat32ArrType_Type;
 }
 
 
