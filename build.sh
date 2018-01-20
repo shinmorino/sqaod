e883f094b003bec7abadc97304dc9a0aca40489f
Shinya Morino <shin.morino@gmail.com>
2018-01-20 22:13:19 +0900

    pulling CUB

--- build.sh
+++ build.sh
@@ -5,9 +5,14 @@ sudo apt-get install automake autoconf libtool python-dev
 pushd .
 
 cd libsqaod
+
 git clone https://github.com/Rlovelett/eigen.git
 cd eigen
 git checkout 3.3.4
+git clone https://github.com/NVlabs/cub.git
+cd cub
+git checkout v1.7.4
+
 cd ..
 ./autogen.sh
 ./configure #--prefix=/usr
