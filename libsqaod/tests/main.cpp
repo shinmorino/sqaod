a1369d6ed1d8905da380a4baf00462c5854d3c3b
Shinya Morino <shin.morino@gmail.com>
2018-01-22 21:56:31 +0900

    Adding CUDAFormulas tests.

--- libsqaod/tests/main.cpp
+++ libsqaod/tests/main.cpp
@@ -1,10 +1,14 @@
 #include "MinimalTestSuite.h"
 #include "DeviceTest.h"
 #include "DeviceMathTest.h"
+#include "CUDAFormulasDGFuncTest.h"
+#include "CUDAFormulasBGFuncTest.h"
 #include <iostream>
 
 int main(int argc, char* argv[]) {
     runTest<DeviceTest>();
     runTest<DeviceMathTest>();
+    runTest<CUDAFormulasDGFuncTest>();
+    runTest<CUDAFormulasBGFuncTest>();
     return MinimalTestSuite::summarize();
 }
