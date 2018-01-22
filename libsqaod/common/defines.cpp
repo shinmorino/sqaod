b267d11096f5b13ce13d3e089e5cedfbfe77a8eb
Shinya Morino <shin.morino@gmail.com>
2018-01-22 12:18:01 +0900

    Fix parameter list forwarding.

--- libsqaod/common/defines.cpp
+++ libsqaod/common/defines.cpp
@@ -1,6 +1,7 @@
 #include "defines.h"
 #include "undef.h"
 
+#include <stdarg.h>
 #include <stdlib.h>
 #include <stdexcept>
 
@@ -11,14 +12,13 @@ void sqaod::__abort(const char *file, unsigned long line) {
     abort();
 }
 
-void sqaod::__abort(const char *file, unsigned long line, const char *msg) {
-    fprintf(stderr, "%s:%d %s\n", file, (int)line, msg);
-    abort();
-}
-
-void sqaod::__abort(const char *file, unsigned long line, const char *format, va_list va) {
+void sqaod::__abort(const char *file, unsigned long line, const char *format, ...) {
     char msg[512];
+
+    va_list va;
+    va_start(va, format);
     vsnprintf(msg, sizeof(msg), format, va);
+    va_end(va);
     fprintf(stderr, "%s:%d %s\n", file, (int)line, msg);
     abort();
 }
@@ -27,14 +27,15 @@ void sqaod::__throwError(const char *file, unsigned long line) {
     __throwError(file, line, "abort");
 }
 
-void sqaod::__throwError(const char *file, unsigned long line, const char *msg) {
+void sqaod::__throwError(const char *file, unsigned long line, const char *format, ...) {
+    char msg[512];
+
+    va_list va;
+    va_start(va, format);
+    vsnprintf(msg, sizeof(msg), format, va);
+    va_end(va);
+    
     char buffer[512];
     snprintf(buffer, sizeof(buffer), "%s:%d %s\n", file, (int)line, msg);
     throw std::runtime_error(buffer);
-}
-
-void sqaod::__throwError(const char *file, unsigned long line, const char *format, va_list va) {
-    char msg[512];
-    vsnprintf(msg, sizeof(msg), format, va);
-    __throwError(file, line, msg);
 }
--- libsqaod/common/defines.h
+++ libsqaod/common/defines.h
@@ -1,19 +1,23 @@
 #ifndef SQAOD_COMMON_DEFINES_H__
 #define SQAOD_COMMON_DEFINES_H__
 
-#include <stdarg.h>
 #include <assert.h>
+
+
+#ifdef __GNUC__
+#define FORMATATTR(stringIdx, firstToCheck) __attribute__((format(printf, stringIdx, firstToCheck)))
+#else
+#define FORMATATTR(stringIdx, firstToCheck)
+#endif
+
 
 namespace sqaod {
 
 typedef unsigned int SizeType;
 typedef int IdxType;
 
-
 void __abort(const char *file, unsigned long line);
-void __abort(const char *file, unsigned long line, const char *msg);
-/* FIXME: add format warning on gcc. */
-void __abort(const char *file, unsigned long line, const char *format, va_list args);
+void __abort(const char *file, unsigned long line, const char *format, ...) FORMATATTR(3, 4);
 
 template <class... Args> inline
 void _abort(const char *file, unsigned long line, Args... args) {
@@ -21,8 +25,7 @@ void _abort(const char *file, unsigned long line, Args... args) {
 }
 
 void __throwError(const char *file, unsigned long line);
-void __throwError(const char *file, unsigned long line, const char *msg);
-void __throwError(const char *file, unsigned long line, const char *format, va_list args);
+void __throwError(const char *file, unsigned long line, const char *format, ...) FORMATATTR(3, 4);
 
 template <class... Args> inline
 void _throwError(const char *file, unsigned long line, Args... args) {
