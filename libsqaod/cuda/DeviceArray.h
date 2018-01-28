2022287296bd070eca22241d0f1783a47a372108
Shinya Morino <shin.morino@gmail.com>
2018-01-28 17:47:51 +0900

    First working version.

--- libsqaod/cuda/DeviceArray.h
+++ libsqaod/cuda/DeviceArray.h
@@ -2,25 +2,33 @@
 #define SQAOD_DEVICEARRAY_H__
 
 #include <common/Array.h>
+#include <cuda/DeviceObject.h>
 
 namespace sqaod_cuda {
 
 template<class V>
-struct DeviceArrayType {
-    typedef DeviceArrayType<V> DeviceArray;
-    DeviceArrayType();
-
-    void append(const DeviceArray &dar);
+struct DeviceArrayType : DeviceObject {
+    DeviceArrayType() : d_data(nullptr), size((sqaod::SizeType)-1) { }
     
-    static
-    void swap(DeviceArray *lhs, DeviceArray *rhs);
-
-    // friend
-    // void merge(DevicePackedBitsArray *lhs, DevicePackedBitsArray *rhs);
-
-
+    DeviceArrayType(const DeviceArrayType<V>&& arr) noexcept {
+        DeviceArrayType newArr;
+        newArr.d_data = arr.d_data;
+        size = arr.size;
+        bufferSize = arr.bufferSize;
+        arr.d_data = NULL;
+    }
+    
     V *d_data;
+    size_t bufferSize;
     size_t size;
+
+private:
+    DeviceArrayType(const DeviceArrayType<V>&);
+    virtual void get_data(void **ppv) { 
+        *ppv = d_data;
+        d_data = NULL;
+        size = (sqaod::SizeType)-1;
+    }
 };
 
 typedef DeviceArrayType<sqaod::PackedBits> DevicePackedBitsArray;
