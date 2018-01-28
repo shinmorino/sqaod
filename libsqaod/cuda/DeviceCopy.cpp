b4978818420de256b6d108cff47a323998340622
Shinya Morino <shin.morino@gmail.com>
2018-01-28 17:44:48 +0900

    Each function is changed to a template.

--- libsqaod/cuda/DeviceCopy.cpp
+++ libsqaod/cuda/DeviceCopy.cpp
@@ -1,132 +1,27 @@
 #include "DeviceCopy.h"
 #include "cudafuncs.h"
 #include "Device.h"
-#include "Assertion.h"
 
 using namespace sqaod_cuda;
-using sqaod::SizeType;
-using sqaod::IdxType;
 
 
-
-template<class real> 
-DeviceCopyType<real>::DeviceCopyType() {
+DeviceCopy::DeviceCopy() {
     devAlloc_ = NULL;
     stream_ = NULL;
 }
 
-template<class real> 
-DeviceCopyType<real>::DeviceCopyType(Device &device, DeviceStream *devStream) {
-    set(device, devStream);
+DeviceCopy::DeviceCopy(Device &device, DeviceStream *devStream) {
+    assignDevice(device, devStream);
 }
 
-template<class real> void DeviceCopyType<real>::
-set(Device &device, DeviceStream *devStream) {
-    devAlloc_ = device.objectAllocator<real>();
+void DeviceCopy::assignDevice(Device &device, DeviceStream *devStream) {
+    devAlloc_ = device.objectAllocator();
     if (devStream != NULL)
         stream_ = devStream->getCudaStream();
     else
         stream_ = NULL;
 }
 
-template<class real> void DeviceCopyType<real>::
-synchronize() const {
+void DeviceCopy::synchronize() const {
     throwOnError(cudaStreamSynchronize(stream_));
 }
-
-
-template<class real> void DeviceCopyType<real>::
-operator()(DeviceMatrix *dst, const HostMatrix &src) {
-    devAlloc_->allocateIfNull(dst, src.dim());
-    assertSameShape(*dst, src, __func__);
-    copy(dst->d_data, src.data, src.rows * src.cols);
-}
-
-template<class real> void DeviceCopyType<real>::
-operator()(HostMatrix *dst, const DeviceMatrix &src) const {
-    if (dst->data == NULL)
-        dst->resize(src.dim());
-    assertSameShape(*dst, src, __func__);
-    copy(dst->data, src.d_data, src.rows * src.cols);
-}
-
-template<class real> void DeviceCopyType<real>::
-operator()(DeviceMatrix *dst, const DeviceMatrix &src) {
-    devAlloc_->allocateIfNull(dst, src.dim());
-    assertSameShape(*dst, src, __func__);
-    copy(dst->d_data, src.d_data, src.rows * src.cols);
-}
-
-template<class real> void DeviceCopyType<real>::
-operator()(DeviceMatrix *dst, const real &src) const {
-    assertValidMatrix(*dst, __func__);
-    copyBroadcast(dst->d_data, src, dst->rows * dst->cols);
-}
-
-template<class real> void DeviceCopyType<real>::
-operator()(DeviceMatrix *dst, const real &src, sqaod::SizeType size,
-           sqaod::SizeType stride, sqaod::IdxType offset) const {
-    assertValidMatrix(*dst, __func__);
-    copyBroadcastStrided(dst->d_data, src, size, stride, offset);
-}
-    
-template<class real> void DeviceCopyType<real>::
-operator()(DeviceVector *dst, const HostVector &src) {
-    devAlloc_->allocateIfNull(dst, src.size);
-    assertSameShape(*dst, src, __func__);
-    copy(dst->d_data, src.data, src.size);
-}
-    
-template<class real> void DeviceCopyType<real>::
-operator()(HostVector *dst, const DeviceVector &src) const {
-    if (dst->data == NULL)
-        dst->allocate(src.size);
-    assertSameShape(*dst, src, __func__);
-    copy(dst->data, src.d_data, src.size);
-}
-
-template<class real> void DeviceCopyType<real>::
-operator()(DeviceVector *dst, const DeviceVector &src) {
-    devAlloc_->allocateIfNull(dst, src.size);
-    assertSameShape(*dst, src, __func__);
-    copy(dst->d_data, src.d_data, src.size);
-}
-
-template<class real> void DeviceCopyType<real>::
-operator()(DeviceVector *dst, const real &src) const {
-    assertValidVector(*dst, __func__);
-    copyBroadcast(dst->d_data, src, 1);
-}
-
-/* Host scalar variables <-> DeviceScalar */
-    
-template<class real> void DeviceCopyType<real>::
-operator()(DeviceScalar *dst, const real &src) {
-    devAlloc_->allocateIfNull(dst);
-    assertValidScalar(*dst, __func__);
-    copy(dst->d_data, &src, 1);
-}
-
-template<class real> void DeviceCopyType<real>::
-operator()(real *dst, const DeviceScalar &src) const {
-    copy(dst, src.d_data, 1);
-}
-
-/* Packed bits */
-template<class real> void DeviceCopyType<real>::
-operator()(sqaod::PackedBitsArray *dst, const DevicePackedBitsArray &src) const {
-    assert(!"not implemented.");
-    // copy(dst->data_, src.d_data, src.size);
-    // dst->size_ = src.size;
-}
-
-template<class real> void DeviceCopyType<real>::
-operator()(DevicePackedBitsArray *dst, const sqaod::PackedBitsArray &src) {
-    assert(!"not implemented.");
-    // copy(dst->data_, src.d_data, src.size);
-    // dst->size_ = src.size;
-}
-
-template struct sqaod_cuda::DeviceCopyType<double>;
-template struct sqaod_cuda::DeviceCopyType<float>;
-
--- libsqaod/cuda/DeviceCopy.h
+++ libsqaod/cuda/DeviceCopy.h
@@ -7,19 +7,15 @@
 #include <cuda/DeviceStream.h>
 #include <cuda/DeviceKernels.h>
 #include <cuda/DeviceObjectAllocator.h>
+#include <cuda/HostObjectAllocator.h>
+#include <cuda/Assertion.h>
+
 
 namespace sqaod_cuda {
 
 class Device;
 
-template<class real>
-struct DeviceCopyType {
-
-    typedef sqaod::MatrixType<real> HostMatrix;
-    typedef sqaod::VectorType<real> HostVector;
-    typedef DeviceMatrixType<real> DeviceMatrix;
-    typedef DeviceVectorType<real> DeviceVector;
-    typedef DeviceScalarType<real> DeviceScalar;
+struct DeviceCopy {
  
     template<class V>
     void copy(V *d_buf, const V *v, sqaod::SizeType nElms) const;
@@ -27,75 +23,186 @@ struct DeviceCopyType {
     template<class V>
     void copyBroadcast(V *d_buf, const V &v, sqaod::SizeType nElms) const;
 
-    void copyBroadcastStrided(real *d_buf, const real &v, sqaod::SizeType size,
+    template<class V>
+    void copyBroadcastStrided(V *d_buf, const V &v, sqaod::SizeType size,
                               sqaod::SizeType stride, sqaod::IdxType offset) const;
-
-    /* HostMatrix <-> DeviceMatrix */
     
-    void operator()(DeviceMatrix *dst, const HostMatrix &src);
+    template<class Vsrc, class Vdst>
+    DeviceMatrixType<Vdst> cast(const DeviceMatrixType<Vsrc> &mat);
+    template<class Vsrc, class Vdst>
+    DeviceVectorType<Vdst> cast(const DeviceVectorType<Vsrc> &vec);
     
-    void operator()(HostMatrix *dst, const DeviceMatrix &src) const;
+    /* sqaod::MatrixType<V> <-> DeviceMatrixType<V> */
     
-    void operator()(DeviceMatrix *dst, const DeviceMatrix &src);
+    template<class V>
+    void operator()(DeviceMatrixType<V> *dst, const sqaod::MatrixType<V> &src);
     
-    void operator()(DeviceMatrix *dst, const real &src) const;
+    template<class V>
+    void operator()(sqaod::MatrixType<V> *dst, const DeviceMatrixType<V> &src) const;
     
-    void operator()(DeviceMatrix *dst, const real &src, sqaod::SizeType size,
+    template<class V>
+    void operator()(DeviceMatrixType<V> *dst, const DeviceMatrixType<V> &src);
+    
+    template<class V>
+    void operator()(DeviceMatrixType<V> *dst, const V &src) const;
+    
+    template<class V>
+    void operator()(DeviceMatrixType<V> *dst, const V &src, sqaod::SizeType size,
                     sqaod::SizeType stride, sqaod::IdxType offset) const;
     
-    /* HostVector <-> DeviceVector */
+    /* sqaod::VectorType<V> <-> DeviceVectorType<V> */
     
-    void operator()(DeviceVector *dst, const HostVector &src);
+    template<class V>
+    void operator()(DeviceVectorType<V> *dst, const sqaod::VectorType<V> &src);
     
-    void operator()(HostVector *dst, const DeviceVector &src) const;
+    template<class V>
+    void operator()(sqaod::VectorType<V> *dst, const DeviceVectorType<V> &src) const;
     
-    void operator()(DeviceVector *dst, const DeviceVector &src);
+    template<class V>
+    void operator()(DeviceVectorType<V> *dst, const DeviceVectorType<V> &src);
     
-    void operator()(DeviceVector *dst, const real &src) const;
+    template<class V>
+    void operator()(DeviceVectorType<V> *dst, const V &src) const;
 
     /* Host scalar variables <-> DeviceScalar */
     
-    void operator()(DeviceScalar *dst, const real &src);
+    template<class V>
+    void operator()(DeviceScalarType<V> *dst, const V &src);
     
-    void operator()(real *dst, const DeviceScalar &src) const;
-    
-    /* Packed bits */
-    void operator()(DevicePackedBitsArray *dst, const sqaod::PackedBitsArray &src);
+    template<class V>
+    void operator()(V *dst, const DeviceScalarType<V> &src) const;
 
-    void operator()(sqaod::PackedBitsArray *dst, const DevicePackedBitsArray &src) const;
+    /* Host array <-> Device Array */
+    template<class V>
+    void operator()(DeviceArrayType<V> *dst, const sqaod::ArrayType<V> &src);
+    
+    template<class V>
+    void operator()(sqaod::ArrayType<V> *dst, const DeviceArrayType<V> &src) const;
+
+    /* Host scalar ref <-> Device Scalar */
 
     void synchronize() const;
 
-    DeviceCopyType();
+    DeviceCopy();
 
-    DeviceCopyType(Device &device, DeviceStream *stream = NULL);
+    DeviceCopy(Device &device, DeviceStream *stream = NULL);
     
-    void set(Device &device, DeviceStream *stream = NULL);
+    void assignDevice(Device &device, DeviceStream *stream = NULL);
     
-
 private:
-    typedef DeviceObjectAllocatorType<real> DeviceObjectAllocator;
     DeviceObjectAllocator *devAlloc_;
     DeviceCopyKernels kernels_;
     cudaStream_t stream_;
 };
 
 
-template<class real> template<class V> inline
-void DeviceCopyType<real>::copy(V *d_buf, const V *v, sqaod::SizeType nElms) const {
+template<class V> inline
+void DeviceCopy::copy(V *d_buf, const V *v, sqaod::SizeType nElms) const {
     throwOnError(cudaMemcpyAsync(d_buf, v, sizeof(V) * nElms, cudaMemcpyDefault, stream_));
 }
 
-template<class real> template<class V> inline
-void DeviceCopyType<real>::copyBroadcast(V *d_buf, const V &v, sqaod::SizeType size) const {
+template<class V> inline
+void DeviceCopy::copyBroadcast(V *d_buf, const V &v, sqaod::SizeType size) const {
     kernels_.copyBroadcast(d_buf, v, size);
 }
 
-template<class real> void DeviceCopyType<real>::
-copyBroadcastStrided(real *d_buf, const real &v, sqaod::SizeType size, sqaod::SizeType stride, sqaod::IdxType offset) const {
+template<class V> void DeviceCopy::
+copyBroadcastStrided(V *d_buf, const V &v, sqaod::SizeType size,
+                     sqaod::SizeType stride, sqaod::IdxType offset) const {
     kernels_.copyBroadcastStrided(d_buf, v, size, stride, offset);
 }
 
+
+template<class V> void DeviceCopy::
+operator()(DeviceMatrixType<V> *dst, const sqaod::MatrixType<V> &src) {
+    devAlloc_->allocateIfNull(dst, src.dim());
+    assertSameShape(*dst, src, __func__);
+    copy(dst->d_data, src.data, src.rows * src.cols);
+}
+
+template<class V> void DeviceCopy::
+operator()(sqaod::MatrixType<V> *dst, const DeviceMatrixType<V> &src) const {
+    if (dst->data == NULL)
+        dst->resize(src.dim());
+    assertSameShape(*dst, src, __func__);
+    copy(dst->data, src.d_data, src.rows * src.cols);
+}
+
+template<class V> void DeviceCopy::
+operator()(DeviceMatrixType<V> *dst, const DeviceMatrixType<V> &src) {
+    devAlloc_->allocateIfNull(dst, src.dim());
+    assertSameShape(*dst, src, __func__);
+    copy(dst->d_data, src.d_data, src.rows * src.cols);
+}
+
+template<class V> void DeviceCopy::
+operator()(DeviceMatrixType<V> *dst, const V &src) const {
+    assertValidMatrix(*dst, __func__);
+    copyBroadcast(dst->d_data, src, dst->rows * dst->cols);
+}
+
+template<class V> void DeviceCopy::
+operator()(DeviceMatrixType<V> *dst, const V &src, sqaod::SizeType size,
+           sqaod::SizeType stride, sqaod::IdxType offset) const {
+    assertValidMatrix(*dst, __func__);
+    copyBroadcastStrided(dst->d_data, src, size, stride, offset);
+}
+    
+template<class V> void DeviceCopy::
+operator()(DeviceVectorType<V> *dst, const sqaod::VectorType<V> &src) {
+    devAlloc_->allocateIfNull(dst, src.size);
+    assertSameShape(*dst, src, __func__);
+    copy(dst->d_data, src.data, src.size);
+}
+    
+template<class V> void DeviceCopy::
+operator()(sqaod::VectorType<V> *dst, const DeviceVectorType<V> &src) const {
+    if (dst->data == NULL)
+        dst->allocate(src.size);
+    assertSameShape(*dst, src, __func__);
+    copy(dst->data, src.d_data, src.size);
+}
+
+template<class V> void DeviceCopy::
+operator()(DeviceVectorType<V> *dst, const DeviceVectorType<V> &src) {
+    devAlloc_->allocateIfNull(dst, src.size);
+    assertSameShape(*dst, src, __func__);
+    copy(dst->d_data, src.d_data, src.size);
+}
+
+template<class V> void DeviceCopy::
+operator()(DeviceVectorType<V> *dst, const V &src) const {
+    assertValidVector(*dst, __func__);
+    copyBroadcast(dst->d_data, src, 1);
+}
+
+/* Host scalar variables <-> DeviceScalar */
+    
+template<class V> void DeviceCopy::
+operator()(DeviceScalarType<V> *dst, const V &src) {
+    devAlloc_->allocateIfNull(dst);
+    copy(dst->d_data, &src, 1);
+}
+
+template<class V> void DeviceCopy::
+operator()(V *dst, const DeviceScalarType<V> &src) const {
+    copy(dst, src.d_data, 1);
+}
+
+/* Packed bits */
+template<class V> void DeviceCopy::
+operator()(sqaod::ArrayType<V> *dst, const DeviceArrayType<V> &src) const {
+    assert(!"not implemented.");
+    // copy(dst->data_, src.d_data, src.size);
+    // dst->size_ = src.size;
+}
+
+template<class V> void DeviceCopy::
+operator()(DeviceArrayType<V> *dst, const sqaod::ArrayType<V> &src) {
+    assert(!"not implemented.");
+    // copy(dst->data_, src.d_data, src.size);
+    // dst->size_ = src.size;
+}
 
 }
 
