2467af5813712f00768b6e1ff8eb35aecea34e75
Shinya Morino <shin.morino@gmail.com>
2018-01-28 17:48:28 +0900

    Each allocatio function is changed to a template.

--- libsqaod/cuda/DeviceObjectAllocator.cpp
+++ libsqaod/cuda/DeviceObjectAllocator.cpp
@@ -2,91 +2,19 @@
 #include "DeviceCopy.h"
 
 using namespace sqaod_cuda;
-using sqaod::Dim;
-
-template<class real>
-const real DeviceObjectAllocatorType<real>::hostConsts_[] = {0., 0.25, 0.5, 1.};
-template<class real>
-const int DeviceObjectAllocatorType<real>::nHostConsts_ = sizeof(hostConsts_) / sizeof(real);
 
 
-template<class real>
-void *DeviceObjectAllocatorType<real>::allocate(size_t size) {
+void *DeviceObjectAllocator::allocate(size_t size) {
     return memStore_->allocate(size);
 }
 
-template<class real>
-void DeviceObjectAllocatorType<real>::deallocate(void *pv) {
+void DeviceObjectAllocator::deallocate(void *pv) {
     return memStore_->deallocate(pv);
 }
 
-template<class real>
-void DeviceObjectAllocatorType<real>::allocate(DeviceMatrix *mat, int rows, int cols) {
-    mat->d_data = (real*)memStore_->allocate(sizeof(real) * rows * cols);
-    mat->rows = rows;
-    mat->cols = cols;
-}
-
-template<class real>
-void DeviceObjectAllocatorType<real>::allocate(DeviceVectorType<real> *vec, int size) {
-    vec->d_data = (real*)memStore_->allocate(sizeof(real) * size);
-    vec->size = size;
-}
-
-template<class real>
-void DeviceObjectAllocatorType<real>::allocate(DeviceScalarType<real> *sc) {
-    sc->d_data = (real*)memStore_->allocate(sizeof(real));
-}
-
-template<class real>
-void DeviceObjectAllocatorType<real>::deallocate(DeviceObject &obj) {
+void DeviceObjectAllocator::deallocate(DeviceObject &obj) {
     void *pv;
     obj.get_data(&pv);
     memStore_->deallocate(pv);
 }
 
-    
-/* Device Const */
-template<class real>
-const DeviceScalarType<real> &DeviceObjectAllocatorType<real>::d_const(const real &c) const {
-    if (c == 1.)
-        return *d_one_;
-    if (c == 0.)
-        return *d_zero_;
-    const real *pos = std::find(hostConsts_, hostConsts_ + nHostConsts_, c);
-    int idx = int(pos - hostConsts_);
-    abortIf(idx == nHostConsts_, "Constant not registered.");
-    return *constReg_[idx];
-}
-
-template<class real>
-void DeviceObjectAllocatorType<real>::initialize(DeviceMemoryStore *memStore,
-                                                 DeviceStream *devStream) {
-    memStore_ = memStore;
-    
-    d_consts_ = (real*)memStore_->allocate(sizeof(real) * nHostConsts_);
-    DeviceCopyType<real>().copy(d_consts_, hostConsts_, nHostConsts_);
-
-    for (int idx = 0; idx < nHostConsts_; ++idx)
-        constReg_.pushBack(new DeviceScalar(&d_consts_[idx]));
-
-    const real *pos = std::find(hostConsts_, hostConsts_ + nHostConsts_, 0.);
-    int idx = int(pos - hostConsts_);
-    d_zero_ = constReg_[idx];
-    pos = std::find(hostConsts_, hostConsts_ + nHostConsts_, 1.);
-    idx = int(pos - hostConsts_);
-    d_one_ = constReg_[idx];
-}
-
-template<class real>
-void DeviceObjectAllocatorType<real>::finalize() {
-    for (int idx = 0; idx < nHostConsts_; ++idx)
-        delete constReg_[idx];
-    
-    constReg_.clear();
-    memStore_->deallocate(d_consts_);
-    d_consts_ = NULL;
-}
-
-template struct sqaod_cuda::DeviceObjectAllocatorType<float>;
-template struct sqaod_cuda::DeviceObjectAllocatorType<double>;
--- libsqaod/cuda/DeviceObjectAllocator.h
+++ libsqaod/cuda/DeviceObjectAllocator.h
@@ -4,93 +4,106 @@
 #include <cuda/DeviceMatrix.h>
 #include <cuda/DeviceMemoryStore.h>
 #include <cuda/DeviceStream.h>
+#include <cuda/DeviceArray.h>
 
 namespace sqaod_cuda {
 
-template<class real>
-struct DeviceObjectAllocatorType {
-    typedef DeviceMatrixType<real> DeviceMatrix;
-    typedef DeviceVectorType<real> DeviceVector;
-    typedef DeviceScalarType<real> DeviceScalar;
+struct DeviceObjectAllocator {
 
     void *allocate(size_t size);
 
     void deallocate(void *pv);
 
-    void allocate(DeviceMatrix *mat, int rows, int cols);
+    template<class V>
+    void allocate(DeviceMatrixType<V> *mat, sqaod::SizeType rows, sqaod::SizeType cols);
 
-    void allocate(DeviceMatrix *mat, const sqaod::Dim &dim);
+    template<class V>
+    void allocate(DeviceMatrixType<V> *mat, const sqaod::Dim &dim);
 
-    void allocate(DeviceVector *vec, int size);
+    template<class V>
+    void allocate(DeviceVectorType<V> *vec, sqaod::SizeType size);
 
-    void allocate(DeviceScalar *sc);
+    template<class V>
+    void allocate(DeviceScalarType<V> *sc);
 
-    void allocateIfNull(DeviceMatrixType<real> *mat, const sqaod::Dim &dim);
+    template<class V>
+    void allocate(DeviceArrayType<V> *arr, sqaod::SizeType size);
+    
+    template<class V>
+    void allocateIfNull(DeviceMatrixType<V> *mat, const sqaod::Dim &dim);
 
-    void allocateIfNull(DeviceVectorType<real> *vec, const sqaod::SizeType size);
+    template<class V>
+    void allocateIfNull(DeviceVectorType<V> *vec, const sqaod::SizeType size);
 
-    void allocateIfNull(DeviceScalarType<real> *sc);
+    template<class V>
+    void allocateIfNull(DeviceScalarType<V> *sc);
+
+    template<class V>
+    void allocateIfNull(DeviceArrayType<V> *arr, sqaod::SizeType size);
     
     void deallocate(DeviceObject &obj);
 
-    /* Device Const */
-    const DeviceScalar &d_const(const real &c) const;
-
-    const DeviceScalar &d_one() const;
-
-    const DeviceScalar &d_zero() const;
-
-    void initialize(DeviceMemoryStore *memStore, DeviceStream *devStream);
-    void finalize();
-
+    void set(DeviceMemoryStore *memStore) {
+        memStore_ = memStore;
+    }
+    
 private:
     DeviceMemoryStore *memStore_;
-    static const real hostConsts_[];
-    static const int nHostConsts_;
-    real *d_consts_;
-
-    typedef sqaod::ArrayType<DeviceScalar*> ConstReg;
-    ConstReg constReg_;
-
-    const DeviceScalar *d_one_;
-    const DeviceScalar *d_zero_;
 };
 
 
-template<class real>
-void DeviceObjectAllocatorType<real>::allocateIfNull(DeviceMatrixType<real> *mat, const sqaod::Dim &dim) {
+template<class V> inline
+void DeviceObjectAllocator::allocate(DeviceMatrixType<V> *mat, const sqaod::Dim &dim) {
+    return allocate(mat, dim.rows, dim.cols);
+}
+
+template<class V>
+void DeviceObjectAllocator::allocate(DeviceMatrixType<V> *mat, sqaod::SizeType rows, sqaod::SizeType cols) {
+    mat->d_data = (V*)allocate(sizeof(V) * rows * cols);
+    mat->rows = rows;
+    mat->cols = cols;
+}
+
+template<class V>
+void DeviceObjectAllocator::allocate(DeviceVectorType<V> *vec, sqaod::SizeType size) {
+    vec->d_data = (V*)allocate(sizeof(V) * size);
+    vec->size = size;
+}
+
+template<class V>
+void DeviceObjectAllocator::allocate(DeviceScalarType<V> *sc) {
+    sc->d_data = (V*)allocate(sizeof(V));
+}
+
+template<class V>
+void DeviceObjectAllocator::allocate(DeviceArrayType<V> *arr, sqaod::SizeType size) {
+    arr->d_data = (V*)allocate(sizeof(V) * size);
+    arr->size = size;
+}
+
+template<class V> inline
+void DeviceObjectAllocator::allocateIfNull(DeviceMatrixType<V> *mat, const sqaod::Dim &dim) {
     if (mat->d_data == NULL)
         allocate(mat, dim);
 }
 
-template<class real>
-void DeviceObjectAllocatorType<real>::allocateIfNull(DeviceVectorType<real> *vec, const sqaod::SizeType size) {
+template<class V> inline
+void DeviceObjectAllocator::allocateIfNull(DeviceVectorType<V> *vec, const sqaod::SizeType size) {
     if (vec->d_data == NULL)
         allocate(vec, size);
 }
 
-template<class real>
-void DeviceObjectAllocatorType<real>::allocateIfNull(DeviceScalarType<real> *sc) {
+template<class V> inline
+void DeviceObjectAllocator::allocateIfNull(DeviceScalarType<V> *sc) {
     if (sc->d_data == NULL)
         allocate(sc);
 }
 
-
-template<class real> inline
-void DeviceObjectAllocatorType<real>::allocate(DeviceMatrix *mat, const sqaod::Dim &dim) {
-    return allocate(mat, dim.rows, dim.cols);
+template<class V> inline
+void DeviceObjectAllocator::allocateIfNull(DeviceArrayType<V> *arr, sqaod::SizeType size) {
+    if (arr->d_data == NULL)
+        allocate(arr, size);
 }
-
-template<class real> inline
-const DeviceScalarType<real> &DeviceObjectAllocatorType<real>::d_one() const {
-    return *d_one_;
-}
-
-template<class real> inline
-const DeviceScalarType<real> &DeviceObjectAllocatorType<real>::d_zero() const {
-    return *d_zero_;
-}
-
 
 }
 
