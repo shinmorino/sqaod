69ea32959e6768c8056ad1c533494f0975ae6960
Shinya Morino <shin.morino@gmail.com>
2018-03-03 14:35:00 +0900

    preference.py is added for solver preference.

--- sqaodpy/sqaod/__init__.py
+++ sqaodpy/sqaod/__init__.py
@@ -1,53 +1,10 @@
 
-class Algorithm :
-    pass
-
-algorithm = Algorithm()
-setattr(algorithm, 'default', 'default')
-setattr(algorithm, 'naive', 'naive')
-setattr(algorithm, 'cloring', 'coloring')
-
-algorithm.default = 'default'
-algorithm.naive = 'naive'
-algorithm.coloring = 'coloring'
-algorithm.brute_force_search = 'brute_force_search'
-
-
-# operations to switch minimize / maximize.
-
-class Minimize :
-    @staticmethod
-    def sign(v) :
-        return v
-    @staticmethod
-    def best(list) :
-        return min(list)
-    @staticmethod
-    def sort(list) :
-        return sorted(list)
-    def __trunc__(self) :
-        return 0
-
-    
-class Maximize :
-    @staticmethod
-    def sign(v) :
-        return -v
-    @staticmethod
-    def best(list) :
-        return max(list)
-    @staticmethod
-    def sort(list) :
-        return sorted(list, reverse = true)
-    def __trunc__(self) :
-        return 1
-
-minimize = Minimize()
-maximize = Maximize()
-
-
 # imports
+from sqaod.common.cuda import is_cuda_available
+from sqaod.common.preference import algorithm
+from sqaod.common.preference import minimize
+from sqaod.common.preference import maximize
+
 from sqaod.common import *
 import sqaod.py
 import sqaod.cpu
-from sqaod.common.cuda import is_cuda_available
---
+++ sqaodpy/sqaod/common/preference.py
@@ -0,0 +1,41 @@
+# operations to switch minimize / maximize.
+
+class Algorithm :
+    pass
+
+algorithm = Algorithm()
+algorithm.default = 'default'
+algorithm.naive = 'naive'
+algorithm.coloring = 'coloring'
+algorithm.brute_force_search = 'brute_force_search'
+
+
+class Minimize :
+    @staticmethod
+    def sign(v) :
+        return v
+    @staticmethod
+    def best(list) :
+        return min(list)
+    @staticmethod
+    def sort(list) :
+        return sorted(list)
+    def __trunc__(self) :
+        return 0
+
+    
+class Maximize :
+    @staticmethod
+    def sign(v) :
+        return -v
+    @staticmethod
+    def best(list) :
+        return max(list)
+    @staticmethod
+    def sort(list) :
+        return sorted(list, reverse = true)
+    def __trunc__(self) :
+        return 1
+
+minimize = Minimize()
+maximize = Maximize()
