608d28386c6cec54be329bfc286367328a8654f2
Shinya Morino <shin.morino@gmail.com>
2018-03-03 15:06:08 +0900

    using c-extensions as data files.

--- sqaodpy/setup.py
+++ sqaodpy/setup.py
@@ -1,48 +1,28 @@
-from setuptools import setup, find_packages, Extension
-import platform
-import numpy
+from setuptools import setup, find_packages, Extension, dist
+import sys
 
-
-def new_cpu_ext(name, srcs) :
-    ext_includes = [numpy.get_include(), '../libsqaod/include', '../libsqaod', '../libsqaod/eigen']
-    ext = Extension(name, srcs,
-                    include_dirs=ext_includes,
-                    extra_compile_args = ['-std=c++11', '-Wno-format-security'],
-                    extra_link_args = ['-L../libsqaod/.libs', '-lsqaod'])
-    return ext
-
-def new_cuda_ext(name, srcs) :
-    ext_includes = [numpy.get_include(), '/usr/local/cuda-9.1/include', '../libsqaod/include', '../libsqaod', '../libsqaod/eigen', '../libsqaod/cub']
-    ext = Extension(name, srcs,
-                    include_dirs=ext_includes,
-                    extra_compile_args = ['-std=c++11', '-Wno-format-security'],
-                    extra_link_args = ['-L../libsqaod/.libs', '-lsqaod_cuda','-lsqaod'])
-    return ext
+#https://stackoverflow.com/questions/35112511/pip-setup-py-bdist-wheel-no-longer-builds-forced-non-pure-wheels
+class BinaryDistribution(dist.Distribution):
+    def has_ext_modules(self):
+        return True
     
-ext_modules = []
-if platform.system() != 'Windows' :
-    ext_modules.append(new_cpu_ext('sqaod.cpu.cpu_dg_bf_searcher', ['sqaod/cpu/src/cpu_dg_bf_searcher.cpp']))
-    ext_modules.append(new_cpu_ext('sqaod.cpu.cpu_dg_annealer', ['sqaod/cpu/src/cpu_dg_annealer.cpp']))
-    ext_modules.append(new_cpu_ext('sqaod.cpu.cpu_bg_bf_searcher', ['sqaod/cpu/src/cpu_bg_bf_searcher.cpp']))
-    ext_modules.append(new_cpu_ext('sqaod.cpu.cpu_bg_annealer', ['sqaod/cpu/src/cpu_bg_annealer.cpp']))
-    ext_modules.append(new_cpu_ext('sqaod.cpu.cpu_formulas', ['sqaod/cpu/src/cpu_formulas.cpp']))
-
-    ext_modules.append(new_cuda_ext('sqaod.cuda.cuda_device', ['sqaod/cuda/src/cuda_device.cpp']))
-    ext_modules.append(new_cuda_ext('sqaod.cuda.cuda_dg_bf_searcher', ['sqaod/cuda/src/cuda_dg_bf_searcher.cpp']))
-    ext_modules.append(new_cuda_ext('sqaod.cuda.cuda_dg_annealer', ['sqaod/cuda/src/cuda_dg_annealer.cpp']))
-    ext_modules.append(new_cuda_ext('sqaod.cuda.cuda_bg_bf_searcher', ['sqaod/cuda/src/cuda_bg_bf_searcher.cpp']))
-    ext_modules.append(new_cuda_ext('sqaod.cuda.cuda_bg_annealer', ['sqaod/cuda/src/cuda_bg_annealer.cpp']))
-    ext_modules.append(new_cuda_ext('sqaod.cuda.cuda_formulas', ['sqaod/cuda/src/cuda_formulas.cpp']))
-
+if sys.platform == 'linux2' :
+    package_data = {'sqaod.cpu' : ['*.so', 'sqaod/cpu/*.so' ], 'sqaod.cuda' : ['*.so', 'sqaod/cuda/*.so' ] }
+else :    
+    package_data = {'sqaod.cpu' : ['*.pyd', 'sqaod/cpu/*.pyd' ], 'sqaod.cuda' : ['*.pyd', 'sqaod/cuda/*.pyd' ] }
+    
 setup(
     name='sqaod',
-    version='0.0.dev0',
+    version='0.1.dev0',
     packages=find_packages(exclude=['tests']),
     install_requires=['numpy>=1.11'],
     author='Shinya Morino',
     author_email="shin.morino_at_gmail.com",
-    description='A collection of solvers for Quantum annealer.',
-    license='BSD 3-Clause License',
+    description='A collection of solvers for simulated quantum annealer.',
+    license='Apache2',
     keywords='quantum annealing solver',
-    ext_modules=ext_modules,
+    #ext_modules=ext_modules,
+    package_data=package_data,
+    include_package_data=True,
+    distclass=BinaryDistribution
 )
