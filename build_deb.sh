c602cc9f3b6de9adce74a1468f0abc9f66ed6cfe
Shinya Morino <shin.morino@gmail.com>
2018-03-11 22:38:07 +0900

    deb configs are renamed as *.gen

--- build_deb.sh
+++ build_deb.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+# FIXME: add Eigen SIMD flags.
+export CFLAGS="-O2"
+
+rm -f debian/*.log
+
 make clean
+cd debian
+cp control.gen control
+cp rules.gen rules
+cd ..
 pushd .
 cd ..
-tar -czf sqaod-0.1.0.tar.gz sqaod-0.1.0
+tar -czf sqaod_0.1.0.orig.tar.gz sqaod-0.1.0
 popd
 dpkg-buildpackage -us -uc
 
