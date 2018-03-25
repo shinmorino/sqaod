96bdddd75efaf024956bfe390592e71103193417
Shinya Morino <shin.morino@gmail.com>
2018-03-25 12:18:49 +0900

    Using gen.py to generate control and *.install.

--- build_deb.sh
+++ build_deb.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 
-# FIXME: add Eigen SIMD flags.
 export CFLAGS="-O2"
 
-rm -f debian/*.log
-rm -rf debian/libsqaodc
-rm -rf debian/libsqaodc-cuda-*
-
-make clean
+rm -rf debian/*
+git checkout debian
 cd debian
-cp control.gen control
-cp rules.gen rules
+python gen.py ${1} ${2}
 cd ..
+
 pushd .
 cd ..
 tar -czf sqaod_0.1.0.orig.tar.gz sqaod-0.1.0
 popd
 dpkg-buildpackage -us -uc
-
-# FIXME: add cuda ver to package name ?
