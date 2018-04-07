7f4a6ba920c5f72efcec1a5aa76b272d98cb5138
Shinya Morino <shin.morino@gmail.com>
2018-04-08 01:52:57 +0900

    #25: using up-to-date version of cub.

--- clone_3rdparty.sh
+++ clone_3rdparty.sh
@@ -8,6 +8,8 @@ then
     git clone https://github.com/eigenteam/eigen-git-mirror.git eigen
 fi
 cd eigen
+git checkout master
+git pull
 git checkout 3.3.4
 cd ..
 
@@ -16,5 +18,7 @@ then
     git clone https://github.com/NVlabs/cub.git
 fi
 cd cub
-git checkout v1.7.4
+git checkout master
+git pull
+git checkout v1.8.0
 cd ..
