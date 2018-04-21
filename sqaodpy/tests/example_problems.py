79cd1c4ae18c86be78c06dd8b72ce7d7dcabfa12
Shinya Morino <shin.morino@gmail.com>
2018-04-21 19:05:50 +0900

    fix_type() applied.

--- sqaodpy/tests/example_problems.py
+++ sqaodpy/tests/example_problems.py
@@ -17,7 +17,7 @@ def dense_graph_random(N, dtype) :
     return sqaod.generate_random_symmetric_W(N, dtype=dtype)
 
 def bipartite_graph_random(N0, N1, dtype = np.float64) :
-    b0 = sqaod.clone_as_ndarray(np.random.random((N0)) - 0.5, dtype)
-    b1 = sqaod.clone_as_ndarray(np.random.random((N1)) - 0.5, dtype)
-    W = sqaod.clone_as_ndarray(np.random.random((N1, N0)) - 0.5, dtype)
+    b0 = sqaod.fix_type(np.random.random((N0)) - 0.5, dtype)
+    b1 = sqaod.fix_type(np.random.random((N1)) - 0.5, dtype)
+    W = sqaod.fix_type(np.random.random((N1, N0)) - 0.5, dtype)
     return b0, b1, W
