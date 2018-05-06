bb45a35c5a29b7e791d5cb3283a7149f1395aadf
Shinya Morino <shin.morino@gmail.com>
2018-05-06 19:11:50 +0900

    Python glues for formulas e-extensions are updated to use public API.

--- sqaodpy/sqaod/common/formulas_base.py
+++ sqaodpy/sqaod/common/formulas_base.py
@@ -9,7 +9,7 @@ import sqaod.common as common
 
 # QUBO energy functions
 
-def dense_graph_calculate_E(W, x, cext, dtype) :
+def dense_graph_calculate_E(cext, cobj, W, x, dtype) :
     checkers.dense_graph.qubo(W)
     checkers.dense_graph.bits(W, x)
     checkers.assert_is_vector('x', x)
@@ -17,10 +17,10 @@ def dense_graph_calculate_E(W, x, cext, dtype) :
     x = common.fix_type(x, np.int8)
 
     E = np.ndarray((1), dtype)
-    cext.dense_graph_calculate_E(E, W, x, dtype)
+    cext.dense_graph_calculate_E(cobj, E, W, x, dtype)
     return E[0]
 
-def dense_graph_batch_calculate_E(W, x, cext, dtype) :
+def dense_graph_batch_calculate_E(cext, cobj, W, x, dtype) :
     checkers.dense_graph.qubo(W);
     checkers.dense_graph.bits(W, x)
     W = common.fix_type(W, dtype)
@@ -28,13 +28,13 @@ def dense_graph_batch_calculate_E(W, x, cext, dtype) :
     x = x.reshape(1, -1) if len(x.shape) == 1 else x
     
     E = np.empty((x.shape[0]), dtype)
-    cext.dense_graph_batch_calculate_E(E, W, x, dtype)
+    cext.dense_graph_batch_calculate_E(cobj, E, W, x, dtype)
     return E
 
 
 # QUBO -> Ising model
 
-def dense_graph_calculate_hamiltonian(W, cext, dtype) :
+def dense_graph_calculate_hamiltonian(cext, cobj, W, dtype) :
     W = common.fix_type(W, dtype)
     checkers.dense_graph.qubo(W)
 
@@ -42,12 +42,12 @@ def dense_graph_calculate_hamiltonian(W, cext, dtype) :
     h = np.empty((N), dtype)
     J = np.empty((N, N), dtype)
     c = np.empty((1), dtype)
-    cext.dense_graph_calculate_hamiltonian(h, J, c, W, dtype);
+    cext.dense_graph_calculate_hamiltonian(cobj, h, J, c, W, dtype);
     return h, J, c[0]
 
 # Ising model energy functions
 
-def dense_graph_calculate_E_from_spin(h, J, c, q, cext, dtype) :
+def dense_graph_calculate_E_from_spin(cext, cobj, h, J, c, q, dtype) :
     checkers.dense_graph.hJc(h, J, c);
     checkers.dense_graph.bits(J, q);
     checkers.assert_is_vector('q', q)
@@ -55,22 +55,22 @@ def dense_graph_calculate_E_from_spin(h, J, c, q, cext, dtype) :
     q = common.fix_type(q, np.int8)
     
     E = np.ndarray((1), dtype)
-    cext.dense_graph_calculate_E_from_spin(E, h, J, c, q, dtype)
+    cext.dense_graph_calculate_E_from_spin(cobj, E, h, J, c, q, dtype)
     return E[0]
 
-def dense_graph_batch_calculate_E_from_spin(h, J, c, q, cext, dtype) :
+def dense_graph_batch_calculate_E_from_spin(cext, cobj, h, J, c, q, dtype) :
     h, J = common.fix_type([h, J], dtype)
     checkers.dense_graph.hJc(h, J, c);
     checkers.dense_graph.bits(J, q);
 
     E = np.empty([q.shape[0]], dtype)
-    cext.dense_graph_batch_calculate_E_from_spin(E, h, J, c, q, dtype)
+    cext.dense_graph_batch_calculate_E_from_spin(cobj, E, h, J, c, q, dtype)
     return E
 
 
 # bipartite_graph
 
-def bipartite_graph_calculate_E(b0, b1, W, x0, x1, cext, dtype) :
+def bipartite_graph_calculate_E(cext, cobj, b0, b1, W, x0, x1, dtype) :
     checkers.bipartite_graph.qubo(b0, b1, W)
     checkers.bipartite_graph.bits(W, x0, x1)
     checkers.assert_is_vector('x0', x0)
@@ -79,10 +79,10 @@ def bipartite_graph_calculate_E(b0, b1, W, x0, x1, cext, dtype) :
     b0, b1, W = common.fix_type([b0, b1, W], dtype)
     x0, x1 = common.fix_type([x0, x1], np.int8)
     E = np.ndarray((1), dtype)
-    cext.bipartite_graph_calculate_E(E, b0, b1, W, x0, x1, dtype)
+    cext.bipartite_graph_calculate_E(cobj, E, b0, b1, W, x0, x1, dtype)
     return E[0]
 
-def bipartite_graph_batch_calculate_E(b0, b1, W, x0, x1, cext, dtype) :
+def bipartite_graph_batch_calculate_E(cext, cobj, b0, b1, W, x0, x1, dtype) :
     checkers.bipartite_graph.qubo(b0, b1, W)
     checkers.bipartite_graph.bits(W, x0, x1)
     b0, b1, W = common.fix_type([b0, b1, W], dtype)
@@ -94,10 +94,10 @@ def bipartite_graph_batch_calculate_E(b0, b1, W, x0, x1, cext, dtype) :
     if nBatch0 == 1 :
         x0, x1 = x0.reshape(1, -1), x1.reshape(1, -1)
     E = np.empty((nBatch0), dtype)
-    cext.bipartite_graph_batch_calculate_E(E, b0, b1, W, x0, x1, dtype)
+    cext.bipartite_graph_batch_calculate_E(cobj, E, b0, b1, W, x0, x1, dtype)
     return E
 
-def bipartite_graph_batch_calculate_E_2d(b0, b1, W, x0, x1, cext, dtype) :
+def bipartite_graph_batch_calculate_E_2d(cext, cobj, b0, b1, W, x0, x1, dtype) :
     checkers.bipartite_graph.qubo(b0, b1, W)
     checkers.bipartite_graph.bits(W, x0, x1)
     b0, b1, W = common.fix_type([b0, b1, W], dtype)
@@ -106,11 +106,11 @@ def bipartite_graph_batch_calculate_E_2d(b0, b1, W, x0, x1, cext, dtype) :
     nBatch0 = 1 if len(x0.shape) == 1 else x0.shape[0]
     nBatch1 = 1 if len(x1.shape) == 1 else x1.shape[0]
     E = np.empty((nBatch1, nBatch0), dtype)
-    cext.bipartite_graph_batch_calculate_E_2d(E, b0, b1, W, x0, x1, dtype)
+    cext.bipartite_graph_batch_calculate_E_2d(cobj, E, b0, b1, W, x0, x1, dtype)
     return E
 
 
-def bipartite_graph_calculate_hamiltonian(b0, b1, W, cext, dtype) :
+def bipartite_graph_calculate_hamiltonian(cext, cobj, b0, b1, W, dtype) :
     checkers.bipartite_graph.qubo(b0, b1, W)
     b0, b1, W = common.fix_type([b0, b1, W], dtype)
 
@@ -120,10 +120,10 @@ def bipartite_graph_calculate_hamiltonian(b0, b1, W, cext, dtype) :
     h1 = np.empty((N1), dtype)
     J = np.empty((N1, N0), dtype)
     c = np.empty((1), dtype)
-    cext.bipartite_graph_calculate_hamiltonian(h0, h1, J, c, b0, b1, W, dtype);
+    cext.bipartite_graph_calculate_hamiltonian(cobj, h0, h1, J, c, b0, b1, W, dtype);
     return h0, h1, J, c[0]
 
-def bipartite_graph_calculate_E_from_spin(h0, h1, J, c, q0, q1, cext, dtype) :
+def bipartite_graph_calculate_E_from_spin(cext, cobj, h0, h1, J, c, q0, q1, dtype) :
     checkers.bipartite_graph.hJc(h0, h1, J, c)
     checkers.bipartite_graph.bits(J, q0, q1)
     checkers.assert_is_vector('q0', q0)
@@ -132,10 +132,10 @@ def bipartite_graph_calculate_E_from_spin(h0, h1, J, c, q0, q1, cext, dtype) :
     q0, q1 = common.fix_type([q0, q1], np.int8)
 
     E = np.ndarray((1), dtype)
-    cext.bipartite_graph_calculate_E_from_spin(E, h0, h1, J, c, q0, q1, dtype)
+    cext.bipartite_graph_calculate_E_from_spin(cobj, E, h0, h1, J, c, q0, q1, dtype)
     return E[0]
 
-def bipartite_graph_batch_calculate_E_from_spin(h0, h1, J, c, q0, q1, cext, dtype) :
+def bipartite_graph_batch_calculate_E_from_spin(cext, cobj, h0, h1, J, c, q0, q1, dtype) :
     checkers.bipartite_graph.hJc(h0, h1, J, c)
     checkers.bipartite_graph.bits(J, q0, q1)
     h0, h1, W = common.fix_type([h0, h1, J], dtype)
@@ -146,5 +146,5 @@ def bipartite_graph_batch_calculate_E_from_spin(h0, h1, J, c, q0, q1, cext, dtyp
     if nBatch0 == 1 :
         q0, q1 = q0.reshape(1, -1), q1.reshape(1, -1)
     E = np.empty((nBatch0), dtype)
-    cext.bipartite_graph_batch_calculate_E_from_spin(E, h0, h1, J, c, q0, q1, dtype)
+    cext.bipartite_graph_batch_calculate_E_from_spin(cobj, E, h0, h1, J, c, q0, q1, dtype)
     return E
--- sqaodpy/sqaod/cuda/formulas.py
+++ sqaodpy/sqaod/cuda/formulas.py
@@ -1,45 +1,62 @@
 from __future__ import print_function
+import sys
 from sqaod.common import formulas_base as base
 from . import cuda_formulas as cext
-from . import device
-
-# initialization
-cext.assign_device(device.active_device._cobj)
 
 # dense graph
 
 def dense_graph_calculate_E(W, x, dtype) :
-    return base.dense_graph_calculate_E(W, x, cext, dtype)
+    return base.dense_graph_calculate_E(cext, _dgobj, W, x, dtype)
 
 def dense_graph_batch_calculate_E(W, x, dtype) :
-    return base.dense_graph_batch_calculate_E(W, x, cext, dtype)
+    return base.dense_graph_batch_calculate_E(cext, _dgobj, W, x, dtype)
 
 def dense_graph_calculate_hamiltonian(W, dtype) :
-    return base.dense_graph_calculate_hamiltonian(W, cext, dtype)
+    return base.dense_graph_calculate_hamiltonian(cext, _dgobj, W, dtype)
 
 def dense_graph_calculate_E_from_spin(h, J, c, q, dtype) :
-    return base.dense_graph_calculate_E_from_spin(h, J, c, q, cext, dtype)
+    return base.dense_graph_calculate_E_from_spin(cext, _dgobj, h, J, c, q, dtype)
 
 def dense_graph_batch_calculate_E_from_spin(h, J, c, q, dtype) :
-    return base.dense_graph_batch_calculate_E_from_spin(h, J, c, q, cext, dtype)
+    return base.dense_graph_batch_calculate_E_from_spin(cext, _dgobj, h, J, c, q, dtype)
 
 
 # bipartite_graph
 
 def bipartite_graph_calculate_E(b0, b1, W, x0, x1, dtype) :
-    return base.bipartite_graph_calculate_E(b0, b1, W, x0, x1, cext, dtype)
+    return base.bipartite_graph_calculate_E(cext, _bgobj, b0, b1, W, x0, x1, dtype)
 
 def bipartite_graph_batch_calculate_E(b0, b1, W, x0, x1, dtype) :
-    return base.bipartite_graph_batch_calculate_E(b0, b1, W, x0, x1, cext, dtype)
+    return base.bipartite_graph_batch_calculate_E(cext, _bgobj, b0, b1, W, x0, x1, dtype)
 
 def bipartite_graph_batch_calculate_E_2d(b0, b1, W, x0, x1, dtype) :
-    return base.bipartite_graph_batch_calculate_E_2d(b0, b1, W, x0, x1, cext, dtype)
+    return base.bipartite_graph_batch_calculate_E_2d(cext, _bgobj, b0, b1, W, x0, x1, dtype)
 
 def bipartite_graph_calculate_hamiltonian(b0, b1, W, dtype) :
-    return base.bipartite_graph_calculate_hamiltonian(b0, b1, W, cext, dtype)
+    return base.bipartite_graph_calculate_hamiltonian(cext, _bgobj, b0, b1, W, dtype)
 
 def bipartite_graph_calculate_E_from_spin(h0, h1, J, c, q0, q1, dtype) :
-    return base.bipartite_graph_calculate_E_from_spin(h0, h1, J, c, q0, q1, cext, dtype)
+    return base.bipartite_graph_calculate_E_from_spin(cext, _bgobj, h0, h1, J, c, q0, q1, dtype)
 
 def bipartite_graph_batch_calculate_E_from_spin(h0, h1, J, c, q0, q1, dtype) :
-    return base.bipartite_graph_batch_calculate_E_from_spin(h0, h1, J, c, q0, q1, cext, dtype)
+    return base.bipartite_graph_batch_calculate_E_from_spin(cext, _bgobj, h0, h1, J, c, q0, q1, dtype)
+
+
+# Global/Static
+
+this_module = sys.modules[__name__]
+
+def unload() :
+    cext.dg_formulas_delete(this_module._dgobj)
+    cext.bg_formulas_delete(this_module._bgobj)
+    this_module._dgobj = None
+    this_module._bgobj = None
+
+if __name__ != "__main__" :
+    this_module._dgobj = cext.dg_formulas_new()
+    this_module._bgobj = cext.bg_formulas_new()
+    # initialization
+    cext.assign_device(this_module._dgobj, device.active_device._cobj)
+    cext.assign_device(this_module._bgobj, device.active_device._cobj)
+    import atexit
+    atexit.register(unload)
