53c79f9f69bb568adeef677ff89eacd986827f30
Shinya Morino <shin.morino@gmail.com>
2018-05-18 07:33:09 +0900

    Adding Dot_SIMD, and applied to CPUDenseGraphAnnealer.cpp

---
+++ sqaodc/cpu/Dot_SIMD.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <sqaodc/common/Common.h>
+
+namespace sqaod_cpu {
+
+namespace sq = sqaod;
+
+#ifdef __AVX2__
+
+double dot_avx2(const double *v0, const double *v1, sq::SizeType N);
+
+float dot_avx2(const float *v0, const float *v1, sq::SizeType N);
+
+#endif
+
+#ifdef __SSE2__
+
+double dot_sse2(const double *v0, const double *v1, sq::SizeType N);
+
+float dot_sse2(const float *v0, const float *v1, sq::SizeType N);
+
+#endif
+
+}
