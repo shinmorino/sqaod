1711abaa01101356fb3b6d3ab63fe7e9eed2661e
Shinya Morino <shin.morino@gmail.com>
2018-06-24 15:29:39 +0900

    Adding symmetrize() tests for CPU solvers.

--- sqaodc/tests/CPUDenseGraphAnnealerTest.cpp
+++ sqaodc/tests/CPUDenseGraphAnnealerTest.cpp
@@ -1,4 +1,5 @@
 #include "CPUDenseGraphAnnealerTest.h"
+#include <cpu/SharedFormulas.h>
 #include "utils.h"
 
 namespace sqcpu = sqaod_cpu;
@@ -20,12 +21,38 @@ void CPUDenseGraphAnnealerTest::tearDown() {
     
 void CPUDenseGraphAnnealerTest::run(std::ostream &ostm) {
     int N = 100;
+
+    testcase("symmetrize") {
+        sq::MatrixType<float> W = sq::MatrixType<float>::zeros(N, N);
+        for (sqaod::SizeType iRow = 0; iRow < N; ++iRow) {
+            for (sqaod::SizeType iCol = iRow; iCol < N; ++iCol) {
+                W(iRow, iCol) = float(iRow * 10 + iCol);
+            }
+        }
+        sq::MatrixType<float> Wsym0 = W;
+        for (sqaod::SizeType iRow = 0; iRow < N; ++iRow) {
+            for (sqaod::SizeType iCol = iRow + 1; iCol < N; ++iCol) {
+                Wsym0(iCol, iRow) = Wsym0(iRow, iCol) = W(iRow, iCol) / 2.;
+            }
+        }
+
+        sq::MatrixType<float> Wsym1 = sqcpu::symmetrize(W);
+        TEST_ASSERT(sq::isSymmetric(Wsym1));
+        TEST_ASSERT(Wsym0 == Wsym1);
+    }
+
+    testcase("symmetrize_1") {
+        sq::MatrixType<float> W = createRandomSymmetricMatrix<float>(N);
+        sq::MatrixType<float> Wsym = sqcpu::symmetrize(W);
+        TEST_ASSERT(sq::isSymmetric(Wsym));
+    }
+
     int m = N;
     sqcpu::CPUDenseGraphAnnealer<float> annealer;
     sq::MatrixType<float> W = testMatSymmetric<float>(N);
     annealer.setQUBO(W);
     annealer.setPreference(sq::pnNumTrotters, m);
-
+    
     testcase("set_q(), N") {
         annealer.prepare();
         
