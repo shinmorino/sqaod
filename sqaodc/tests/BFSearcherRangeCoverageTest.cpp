b0fb6e76b40c99d0eab734bb6af156dc14c09a5e
Shinya Morino <shin.morino@gmail.com>
2018-07-01 11:29:53 +0900

    Updated to use public interface.

--- sqaodc/tests/BFSearcherRangeCoverageTest.cpp
+++ sqaodc/tests/BFSearcherRangeCoverageTest.cpp
@@ -21,60 +21,64 @@ void BFSearcherRangeCoverageTest::tearDown() {
 void BFSearcherRangeCoverageTest::run(std::ostream &ostm) {
 #ifdef SQAODC_ENABLE_RANGE_COVERAGE_TEST
     testcase("DenseGraphBFSearcher") {
-        sqaod::cpu::DenseGraphBFSearcher<float> searcher;
+        sqaod::DenseGraphBFSearcher<float> *searcher = sqaod::cpu::newDenseGraphBFSearcher<float>();
         sq::MatrixType<float> W = createRandomSymmetricMatrix<float>(12);
-        searcher.setPreference(sq::Preference(sq::pnTileSize, 61));
-        searcher.setQUBO(W);
-        searcher.prepare();
-        searcher.search();
-        searcher.makeSolution();
+        searcher->setPreference(sq::Preference(sq::pnTileSize, 61));
+        searcher->setQUBO(W);
+        searcher->prepare();
+        searcher->search();
+        searcher->makeSolution();
+        sqaod::deleteInstance(searcher);
         TEST_ASSERT(true);
     }
     testcase("BipartiteGraphBFSearcher") {
         sq::SizeType N0 = 12;
         sq::SizeType N1 = 10;
-        sqaod::cpu::BipartiteGraphBFSearcher<float> searcher;
+        sqaod::BipartiteGraphBFSearcher<float> *searcher = sqaod::cpu::newBipartiteGraphBFSearcher<float>();
         sqaod::VectorType<float> b0 = testVec<float>(N0);
         sqaod::VectorType<float> b1 = testVec<float>(N1);
         sq::MatrixType<float> W = testMat<float>(sq::Dim(N1, N0));
 
-        searcher.setPreference(sq::Preference(sq::pnTileSize0, 61));
-        searcher.setPreference(sq::Preference(sq::pnTileSize1, 37));
-        searcher.setQUBO(b0, b1, W);
-        searcher.prepare();
-        searcher.search();
-        searcher.makeSolution();
+        searcher->setPreference(sq::Preference(sq::pnTileSize0, 61));
+        searcher->setPreference(sq::Preference(sq::pnTileSize1, 37));
+        searcher->setQUBO(b0, b1, W);
+        searcher->prepare();
+        searcher->search();
+        searcher->makeSolution();
+        sqaod::deleteInstance(searcher);
         TEST_ASSERT(true);
     }
 #ifdef SQAODC_CUDA_ENABLED
     sqcu::Device device;
     device.initialize();
     testcase("DenseGraphBFSearcher") {
-        sqaod::cuda::DenseGraphBFSearcher<float> searcher;
-        searcher.assignDevice(device);
+        sqaod::cuda::DenseGraphBFSearcher<float> *searcher = sqaod::cuda::newDenseGraphBFSearcher<float>();
+        searcher->assignDevice(device);
         sq::MatrixType<float> W = createRandomSymmetricMatrix<float>(12);
-        searcher.setPreference(sq::Preference(sq::pnTileSize, 61));
-        searcher.setQUBO(W);
-        searcher.prepare();
-        searcher.search();
-        searcher.makeSolution();
+        searcher->setPreference(sq::Preference(sq::pnTileSize, 61));
+        searcher->setQUBO(W);
+        searcher->prepare();
+        searcher->search();
+        searcher->makeSolution();
+        sqaod::deleteInstance(searcher);
         TEST_ASSERT(true);
     }
     testcase("BipartiteGraphBFSearcher") {
         sq::SizeType N0 = 12;
         sq::SizeType N1 = 10;
-        sqaod::cuda::BipartiteGraphBFSearcher<float> searcher;
-        searcher.assignDevice(device);
+        sqaod::cuda::BipartiteGraphBFSearcher<float> *searcher = sqaod::cuda::newBipartiteGraphBFSearcher<float>();
+        searcher->assignDevice(device);
         sqaod::VectorType<float> b0 = testVec<float>(N0);
         sqaod::VectorType<float> b1 = testVec<float>(N1);
         sq::MatrixType<float> W = testMat<float>(sq::Dim(N1, N0));
 
-        searcher.setPreference(sq::Preference(sq::pnTileSize0, 61));
-        searcher.setPreference(sq::Preference(sq::pnTileSize1, 37));
-        searcher.setQUBO(b0, b1, W);
-        searcher.prepare();
-        searcher.search();
-        searcher.makeSolution();
+        searcher->setPreference(sq::Preference(sq::pnTileSize0, 61));
+        searcher->setPreference(sq::Preference(sq::pnTileSize1, 37));
+        searcher->setQUBO(b0, b1, W);
+        searcher->prepare();
+        searcher->search();
+        searcher->makeSolution();
+        sqaod::deleteInstance(searcher);
         TEST_ASSERT(true);
     }
     device.finalize();
