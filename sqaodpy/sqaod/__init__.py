80d7977f520a0c3b54de20c49e606a9868875cba
Shinya Morino <shin.morino@gmail.com>
2018-07-22 11:03:45 +0900

    Updating version checkers.

--- sqaodpy/sqaod/__init__.py
+++ sqaodpy/sqaod/__init__.py
@@ -1,10 +1,22 @@
+from __future__ import print_function
+
+__version__ = '0.3.1'
+
+from .common.envcheck import *
+from .common.cuda_probe import is_cuda_available
+
+def run_env_check() :
+    print('CUDA : {}'.format('Available' if is_cuda_available() else 'Not available'))
+
+    checker = EnvChecker(__version__)
+    checker.check()
+    checker.show()
+
 try :
     # imports
-    from .common.cuda_probe import is_cuda_available
     from .common.preference import algorithm
     from .common.preference import minimize
     from .common.preference import maximize
-    from .common.envcheck import *
 
     from .common import *
     from . import py
@@ -14,5 +26,8 @@ try :
         from . import cuda
     
 except:
-    from .common.envcheck import *
-    envcheck()
+    checker = EnvChecker(__version__)
+    if not checker.check() :
+        print('\nWrong installation of sqaod backend libraries.')
+        print('Please visit https://github.com/shinmorino/sqaod/wiki/installation to check installation instruction.\n')
+        checker.show()
