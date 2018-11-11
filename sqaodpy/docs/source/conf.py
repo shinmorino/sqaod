9229c9483695c040dd8c9eac4e33f14385b38e22
Shinya Morino <shin.morino@gmail.com>
2018-11-11 15:35:33 +0900

    Documents updated for 1.0.2.

--- sqaodpy/docs/source/conf.py
+++ sqaodpy/docs/source/conf.py
@@ -30,7 +30,7 @@ author = u'Shinya Morino'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'1.0'
+release = u'1.0.2'
 
 
 # -- General configuration ---------------------------------------------------
