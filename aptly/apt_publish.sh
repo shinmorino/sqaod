d71c23143867d4d50b3ce36aeb27e5ecdca72f12
Shinya Morino <shin.morino@gmail.com>
2018-11-17 13:22:44 +0900

    update repository publish/drop scripts.  artful repo dropped, bionic repo added.

--- aptly/apt_publish.sh
+++ aptly/apt_publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 aptly -config=aptly.conf publish repo sqaod_xenial filesystem:root:ubuntu
-aptly -config=aptly.conf publish repo sqaod_artful filesystem:root:ubuntu
+aptly -config=aptly.conf publish repo sqaod_bionic filesystem:root:ubuntu
 
 
--- aptly/apt_publish_drop.sh
+++ aptly/apt_publish_drop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 aptly -config=aptly.conf publish drop xenial filesystem:root:ubuntu
-aptly -config=aptly.conf publish drop artful filesystem:root:ubuntu
+aptly -config=aptly.conf publish drop bionic filesystem:root:ubuntu
